7868dfe30a082351f69b98958bc250093048678a
Md. Mehadee Hasan <69212670+mehadeehasan@users.noreply.github.com>
2023-07-11 22:51:36 +0600

    Create content.jsx

---
+++ src/components/content/content.jsx
@@ -0,0 +1,18 @@
+const Content = () => {
+    return (
+      <div>
+        <h1>Module 06 Assignment</h1>
+        <p>
+
+        Create a new React application using Create React App or any other preferred method.
+        The application should consist of a single page with a header, a content section, and a footer.
+        The header should display the title of your application.
+        The content section should contain a button and a text area.
+        When the button is clicked, the text in the text area should be displayed below the button.
+        The footer should display some information about yourself or your favorite quote.
+        </p>
+      </div>
+    );
+};
+
+export default Header;
