e24894b4d1766363f933a05461494a5eb7854490
Md. Mehadee Hasan <69212670+mehadeehasan@users.noreply.github.com>
2023-07-11 23:39:09 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <div className='container'>
+      <App />
+    </div>
   </React.StrictMode>,
 )
