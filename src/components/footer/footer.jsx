572c495ea4fe02a134d18b6881c0667e8cf6a9ed
Md. Mehadee Hasan <69212670+mehadeehasan@users.noreply.github.com>
2023-07-11 23:53:33 +0600

    Create footer.jsx

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+      <div>
+        <h1>Footer Information </h1>
+        <p>Mehadee Hasan, Mastering on React JS, Next JS & Prisma, Batch 01</p>
+      </div>
+    );
+};
+
+export default Footer;
